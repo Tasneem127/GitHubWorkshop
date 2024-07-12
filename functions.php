7a8a472f67d3be6492e57bc6d3fa33b0c78f3bc1
tasneem <oat7tao@gmail.com>
2024-07-13 00:06:26 +0300

    add  reverseString function

--- functions.php
+++ functions.php
@@ -4,3 +4,8 @@ function calculateRectangleArea($length, $width) {
     $area = $length * $width;
     return $area;
 }
+
+//2- Function to reverse a string
+function reverseString($str) {
+    return strrev($str);
+}
